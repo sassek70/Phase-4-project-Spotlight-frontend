8ac61bfd5445744e6609f495d4306002c0ac75d4
Kevin Sasse <sassek70@gmail.com>
2022-12-06 13:48:15 -0500

    adding form

--- src/Components/LogInForm.js
+++ src/Components/LogInForm.js
@@ -1,14 +1,51 @@
+import { useState } from "react"
 
 
 const LogInForm = () => {
+    const [signup, setSignUp] = useState(false)
+    const [formData, setFormData] = useState({
+        username: "",
+        password: ""
+    })
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setFormData((formData) => ({...formData, [name]: value}))
+    }
+    console.log(formData)
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        let url = 'http://localhost:3000/login'
+        if (signup) url = 'http://localhost:3000/users'
+        fetch(url,{method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ formData }),
+      })
+        .then((r) => r.json())
+        .then((user) => {
+        console.log(user)
+        // onLogin(user);
+    })
+    setFormData({username: "", password: ""})
+    }
+
+
+
     return (
-        <form>
-            {/* <label htmlFor="username">Username:</label>
-            <input type="text" value={formData.username} name="username" placeholder="Enter a Username"></input>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username:</label>
+            <input type="text" value={formData.username} name="username" placeholder="Enter a Username" onChange={handleChange}></input>
 
             <label htmlFor="password">Password:</label>
-            <input type="text" value={formData.password} name="password" placeholder="Enter a password"></input>
-             */}
+            <input type="text" value={formData.password} name="password" placeholder="Enter a password" onChange={handleChange}></input>
+
+            <button type="submit" onClick={() => setSignUp(true)}>Log In</button>
+            
         </form>
     )
 }
